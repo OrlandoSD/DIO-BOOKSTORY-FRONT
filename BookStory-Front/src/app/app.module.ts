86186e25ef5072df6dfdf477d5472040537fade9
OrlandoSD <py2osd.orlando@gmail.com>
2022-06-03 16:49:49 -0300

    criando as pastas do projeto

--- BookStory-Front/src/app/app.module.ts
+++ BookStory-Front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoryAppComponent } from './components/bookstory-app/bookstory-app.component';
+import { FiltersComponent } from './components/bookstory-app/filters/filters.component';
+import { ProductListComponent } from './components/bookstory-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookstoryAppComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
